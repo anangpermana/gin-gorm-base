34a742f0b165344062be67699a96992571741d9e
Anang Permana <anang143@gmail.com>
2023-05-06 12:34:51 +0700

    base setup

---
+++ migrate/migrate.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/anangpermana/gin-gorm-base/initializers"
+	"github.com/anangpermana/gin-gorm-base/models"
+)
+
+func init() {
+	config, err := initializers.LoadConfig(".")
+	if err != nil {
+		log.Fatal("? Could not load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+}
+
+func main() {
+	initializers.DB.AutoMigrate(&models.User{})
+	fmt.Println("? Migration complete")
+}
